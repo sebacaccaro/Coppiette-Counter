0e2c62e88328b1d33104c40c39dc317fff39bbdd
sebacaccaro <sebastianocaccaro@gmail.com>
2020-02-05 15:28:21 +0100

    Added spread module

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 from telegram.ext import Updater, CommandHandler
+from SheetsModule import addCoppietta as add, noCpp
 import logging
 
 
@@ -12,14 +13,20 @@ def start(update, context):
 
 
 def addCoppietta(update, context):
+    res = add()
+    message = "Ahia, cominciamo male! Prima coppietta!" if res == 1 else "Coppietta numero {} di oggi".format(
+        res)
     context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Ho aggiunto una nuova coppietta!"
+        chat_id=update.effective_chat.id, text=message
     )
 
 
 def noCoppietta(update, context):
+    res = noCpp()
+    message = "Ottimo, per ora todo bien" if res != -1 \
+        else "Vecchio, ne hai già segnata almeno una prima"
     context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Ottimo, nessuna coppietta oggi!"
+        chat_id=update.effective_chat.id, text=message
     )
 
 
---
+++ SheetsModule.py
@@ -0,0 +1,40 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import date
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    '../coppiettebot-861ce3e90b40.json', scope)
+gc = gspread.authorize(credentials)
+
+
+def getSheet():
+    return gc.open("Coppiometro").sheet1
+
+
+def dateToday():
+    return date.today().strftime("%d/%m/%Y")
+
+
+def noCpp():
+    sheet = getSheet()
+    dates = sheet.col_values(1)
+    if dates[-1] != dateToday():
+        sheet.update_cell(len(dates)+1, 1, dateToday())
+        sheet.update_cell(len(dates)+1, 2, 0)
+        return 0
+    return -1
+
+
+def addCoppietta():
+    sheet = getSheet()
+    dates = sheet.col_values(1)
+    if dates[-1] != dateToday():
+        sheet.update_cell(len(dates)+1, 1, dateToday())
+        sheet.update_cell(len(dates)+1, 2, 1)
+        return 1
+    else:
+        currentCopNum = int(sheet.col_values(2)[-1])
+        sheet.update_cell(len(dates), 2, currentCopNum+1)
+        return currentCopNum + 1
